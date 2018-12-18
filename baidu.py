f1ae9807668137d743589b9add8d87a4f037d3ad
louis cheung <33168891+LouisCheung85@users.noreply.github.com>
2018-12-18 09:37:52 +0800

    Update baidu.py
    修改wd.

--- baidu.py
+++ baidu.py
@@ -1,6 +1,6 @@
 import urllib.request
 def main():
-	keyword=urllib.parse.urlencode({"wd":"胡锦涛"})
+	keyword=urllib.parse.urlencode({"wd":"test"})
 	#keyword=keyword.encode("utf-8")
 	url="http://www.baidu.com/s?%s" % keyword
 	
